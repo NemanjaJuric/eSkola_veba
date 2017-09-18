63794ac105535714c018e8e46725a6392ac6ac89
Nemanja Juric <nemanjajuric92@gmail.com>
2017-09-18 20:57:53 +0200

    Added angular 4 course

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
@@ -7,6 +7,8 @@ import { Lesson } from '../../classes/lesson';
 import { RouteService } from '../../services/route.service';
 import { SchoolService } from '../../services/school.service';
 
+declare var $: any;
+
 @Component({
   selector: 'book',
   templateUrl: './book.component.html'
@@ -14,21 +16,32 @@ import { SchoolService } from '../../services/school.service';
 export class BookComponent implements OnInit {
 
   constructor(
-    private schoolService: SchoolService, 
+    private schoolService: SchoolService,
     private routeService: RouteService,
-    private router: Router
-    ) { };
+    private router: Router,
+    private elmRef: ElementRef
+  ) { };
 
   private previous: string;
   private next: string;
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd && this.routeService.course != null && this.routeService.lesson != null) {
+        this.initComponent();
+      }
+    });
     this.initComponent();
   }
 
   initComponent() {
     this.getLessonText(this.routeService.getCourseRoute());
     this.getLessonSiblings(this.routeService.lesson, this.routeService.course);
+    this.scrollLesson();
+  }
+
+  scrollLesson() {
+    $(this.elmRef.nativeElement).find('.lesson-text').scrollTop(0);
   }
 
   private lessonText: string;
